281805c84a7e550188ccb11c023be6233edccca0
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2023-07-03 23:53:40 +0300

    ADDED: stylies to Skills component

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -47,6 +47,14 @@ const Backend = () => {
                             <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">SQL</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

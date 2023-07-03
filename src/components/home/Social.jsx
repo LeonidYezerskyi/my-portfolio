26c7669d416168aae8d3f74e01b5802ab6981e24
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2023-07-03 19:53:29 +0300

    CREATED: Home component and stylized it

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.linkedin.com/in/leonid-yezerskyi/" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-linkedin-alt"></i>
+            </a>
+            <a href="https://github.com/LeonidYezerskyi" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-github-alt"></i>
+            </a>
+            <a href="https://www.instagram.com/leonid.yezerskyi/" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-instagram"></i>
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=100000057225766" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-facebook-f"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social

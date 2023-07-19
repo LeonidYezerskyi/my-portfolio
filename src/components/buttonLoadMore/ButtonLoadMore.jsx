d731a7032695750a155fcb5e6853fbcd5f4f3a6a
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2023-07-19 12:56:18 +0300

    ADDED: pagination in portfolio section

---
+++ src/components/buttonLoadMore/ButtonLoadMore.jsx
@@ -0,0 +1,8 @@
+
+const ButtonLoadMore = ({ onClick }) => {
+    return (
+        <button onClick={onClick} className="button button--flex" type="button">Load more</button>
+    )
+};
+
+export default ButtonLoadMore;
--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -2,39 +2,49 @@ import { useState, useEffect } from "react";
 import { projectsData } from "./Data";
 import { projectsNav } from "./Data";
 import WorkItems from "./WorkItems";
+import ButtonLoadMore from "../buttonLoadMore/ButtonLoadMore";
 
 const Works = () => {
-    const [item, setItem] = useState({ name: 'all' });
+    const [item, setItem] = useState({ name: "all" });
     const [projects, setProjects] = useState([]);
     const [active, setActive] = useState(0);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
-        if (item.name === 'all') {
-            setProjects(projectsData);
+        if (item.name === "all") {
+            setProjects(projectsData.slice(0, page * 3 + 3));
         } else {
-            const newProjects = projectsData.filter((project) => {
-                if (item.name === 'single project') {
-                    return (
-                        project.category.includes('single project')
-                    );
-                } else if (item.name === 'team project') {
-                    return (
-                        project.category.includes('team project')
-                    );
-                } else if (item.name === 'backend') {
-                    return (
-                        project.category.includes('backend')
-                    );
+            const filteredProjects = projectsData.filter((project) => {
+                if (item.name === "single project") {
+                    return project.category.includes("single project");
+                } else if (item.name === "team project") {
+                    return project.category.includes("team project");
+                } else if (item.name === "backend") {
+                    return project.category.includes("backend");
                 }
             });
-            setProjects(newProjects);
+
+            const startIndex = (page - 1) * 3;
+            const endIndex = startIndex + 3;
+
+            setProjects(filteredProjects.slice(0, endIndex));
         }
-    }, [item]);
+    }, [item, page]);
 
     const handleClick = (e, index) => {
         setItem({ name: e.target.textContent.toLowerCase() });
         setActive(index);
+        setPage(1);
     };
+
+    const onClickBtn = () => {
+        setPage((page) => page + 1);
+    };
+
+    const showLoadMoreButton =
+        item.name === "all"
+            ? projects.length < projectsData.length
+            : projects.length < projectsData.filter((project) => project.category.includes(item.name)).length;
 
     return (
         <div>
@@ -45,7 +55,7 @@ const Works = () => {
                             onClick={(e) => {
                                 handleClick(e, index);
                             }}
-                            className={`${active === index ? 'active-work' : ''} work__item`}
+                            className={`${active === index ? "active-work" : ""} work__item`}
                             key={index}
                         >
                             {item.name}
@@ -59,9 +69,12 @@ const Works = () => {
                     return <WorkItems item={item} key={item.id} />;
                 })}
             </div>
+
+            <div className="work_btn-load container">
+                {showLoadMoreButton && <ButtonLoadMore onClick={onClickBtn} />}
+            </div>
         </div>
     );
 };
 
 export default Works;
-
